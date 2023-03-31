3e45b9fb0554e2f81e1d362e5b40e2afd3e389a7
Fluke <hunterx52110@gmail.com>
2023-03-31 21:05:47 +0700

    update

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { Usestate } from "react";
+import "./App.css";
+import Button from "./components/button";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Button />
+    </div>
+  );
+};
+
+export default App;
