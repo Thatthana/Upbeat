0fb0bde2668fbe510a5390d1b86a2e2135666335
Fluke <hunterx52110@gmail.com>
2023-04-01 06:29:12 +0700

    First page completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import React, { Usestate } from "react";
 import "./App.css";
 import Button from "./components/button";
+import { BUTTON_TYPES } from "./data/button";
 
 const App = () => {
   return (
     <div className="app">
-      <Button />
+      <img src="picture/upbeat.png" className="logo" alt="" />
+      <div className="button-wrapper">
+        <Button type={BUTTON_TYPES.PRIMARY} btnText="Start" />
+        <Button type={BUTTON_TYPES.SECONDARY} btnText="Configs" />
+        <Button type={BUTTON_TYPES.TERTIARY} btnText="How to play ?" />
+      </div>
     </div>
   );
 };
--- frontend/src/components/button/index.js
+++ frontend/src/components/button/index.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { BUTTON_TYPES } from "../../data/button";
 import "./button.css";
 
-const Button = () => {
-  return <div>Start</div>;
+const Button = (props) => {
+  const { type, btnText } = props;
+  const getButtonClass = () => {
+    switch (type) {
+      case BUTTON_TYPES.PRIMARY:
+        return "primaryBtn button";
+
+      case BUTTON_TYPES.SECONDARY:
+        return "secondaryBtn button";
+      default:
+        return "tertiaryBtn button";
+    }
+  };
+  return <div className={`${getButtonClass()}`}>{btnText}</div>;
 };
 export default Button;
